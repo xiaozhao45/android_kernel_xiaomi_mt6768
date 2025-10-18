394eb262cacd568ed69c79734b8484af8cf62884
MD. Jadly Bin Salam Shovon <shovon129131@gmail.com>
2025-10-18 15:43:46 +0600

    kernel: More fixes and optimization for SUS_MOUNTS and AUTO_ADD_TRY_UMOUNT_FOR_BIND_MOUNT
    - Keep monitoring for KSU process in vfs_create_mount() all the time so that all new ksu mounts can be flagged as sus mounts and assigned with fake mnt_id. This should not cause significant performance loss.
    
    - Keep monitoring the fake mnt_id of old mounts in clone_mnt() all the time so that all new unshared cloned sus mounts and bind mounted sus mounts can be assigned with fake mnt_id. This also should not cause significant performance loss.
    
    - Fixed fake mnt_id of non-umounted sus mounts gets reordered in susfs_reorder_mnt_id()
    
    - Force to run susfs_auto_add_try_umount_for_bind_mount() in do_loopback() as long as it is from ksu process
    
    - Remove restriction of adding duplicate mounts to try_umount list, this should be safe as it is from ksu process
    
    - Increase the value of DEFAULT_KSU_MNT_ID and DEFAULT_KSU_MNT_GROUP_ID just to be more safe as some devices like oneplus has bigger mnt_id for user app process.

--- fs/namespace.c
+++ fs/namespace.c
@@ -1226,17 +1226,11 @@ vfs_kern_mount(struct file_system_type *type, int flags, const char *name, void 
 		return ERR_PTR(-ENODEV);
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	// - We do not check anymore if boot-completed stage is triggered
-	//   just to stop the performance loss
-	if (susfs_is_boot_completed_triggered) {
-		goto orig_flow;
-	}
-	// We only check for ksu process
+	// We keep checking for ksu process
 	if (susfs_is_current_ksu_domain()) {
 		mnt = susfs_alloc_sus_vfsmnt(name);
 		goto bypass_orig_flow;
 	}
-orig_flow:
 #endif
 	mnt = alloc_vfsmnt(name);
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
@@ -1305,10 +1299,10 @@ static struct mount *clone_mnt(struct mount *old, struct dentry *root,
 	struct mount *mnt;
 	int err;
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	// - We do not check anymore if boot-completed stage is triggered
+	// - We do not check anymore for ksu process if boot-completed stage is triggered
 	//   just to stop the performance loss
 	if (susfs_is_boot_completed_triggered) {
-		goto orig_flow;
+		goto skip_checking_for_ksu_proc;
 	}
 
 	// First we must check for ksu process because of magic mount
@@ -1323,13 +1317,12 @@ static struct mount *clone_mnt(struct mount *old, struct dentry *root,
 		goto bypass_orig_flow;
 	}
 
+skip_checking_for_ksu_proc:
 	// Lastly for other processes of which old->mnt_id == DEFAULT_KSU_MNT_ID, go assign fake mnt_id
 	if (old->mnt_id == DEFAULT_KSU_MNT_ID) {
 		mnt = susfs_alloc_sus_vfsmnt(old->mnt_devname);
 		goto bypass_orig_flow;
 	}
-
-orig_flow:
 #endif
 	mnt = alloc_vfsmnt(old->mnt_devname);
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
